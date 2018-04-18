26b4a5b2e1f4367675eceb16567e6f508010e97d
halenv <halenv@rose-hulman.edu>
2018-04-17 21:32:26 -0400

    Not a pretty answer, but it works. Exam 2 problem 2 done

--- src/problem2.py
+++ src/problem2.py
@@ -2,8 +2,8 @@
 Exam 1, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  March 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Noelle Hale.  March 2018.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -154,10 +154,21 @@ def problem2(n, seq):
       :type seq:  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
+    all_list = []
+    first_three = []
+    for k in range(len(seq)):
+        if seq[k] < n:
+            all_list = all_list + [seq[k]]
+    if len(all_list) >= 3:
+        for k in range(3):
+            first_three = first_three + [all_list[k]]
+    else:
+        return 'Too few'
+    return first_three
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
